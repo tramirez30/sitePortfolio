44d8795e1ae29923534354e3c4671ae895947931
tramirez30 <ramirez.tomas@yahoo.com>
2025-12-18 22:52:52 -0800

    fix: adjust hamburger menu logic

--- js/main.js
+++ js/main.js
@@ -7,3 +7,26 @@ document.querySelectorAll("[data-scroll]").forEach((link) => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// Mobile Menu
+const toggleButton = document.getElementById("menuToggle");
+const menu = document.getElementById("mobileMenu");
+
+toggleButton.addEventListener("click", () => {
+  // Only run on mobile
+  if (window.innerWidth >= 1024) return;
+
+  const isOpen = toggleButton.getAttribute("aria-expanded") === "true";
+
+  toggleButton.setAttribute("aria-expanded", String(!isOpen));
+
+  if (isOpen) {
+    // Close menu
+    menu.classList.add("translate-y-[-100%]", "opacity-0");
+    menu.classList.remove("translate-y-0", "opacity-100");
+  } else {
+    // Open menu
+    menu.classList.remove("translate-y-[-100%]", "opacity-0");
+    menu.classList.add("translate-y-0", "opacity-100");
+  }
+});
